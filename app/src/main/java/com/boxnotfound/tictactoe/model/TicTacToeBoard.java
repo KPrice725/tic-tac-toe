741d60a526bc2760a5b4148dea1a75175a383b0d
Kevin Mark Price <KPrice725@aol.com>
2019-05-11 19:56:00 -0400

    refactor: Modify setup method to return cached board
    Only setup a new board if the user directly calls for it, or when the
    application first launches.  In all other cases, just return the
    existing board.

--- app/src/main/java/com/boxnotfound/tictactoe/model/TicTacToeBoard.java
+++ app/src/main/java/com/boxnotfound/tictactoe/model/TicTacToeBoard.java
@@ -10,9 +10,11 @@ public class TicTacToeBoard {
         //prevent instantiation to prevent multiple game board instantiations
     }
 
-    public static TicTacToeTile[][] setupTicTacToeBoard(@IntRange(from = 4) final int rowSize) {
-        gameBoard = new TicTacToeTile[rowSize][rowSize];
-        initializeTiles(rowSize);
+    public static TicTacToeTile[][] setupTicTacToeBoard(@IntRange(from = 4) final int rowSize, final boolean newGame) {
+        if (gameBoard == null || newGame) {
+            gameBoard = new TicTacToeTile[rowSize][rowSize];
+            initializeTiles(rowSize);
+        }
         return gameBoard;
     }
 
@@ -22,7 +24,7 @@ public class TicTacToeBoard {
     private static void initializeTiles(final int rowSize) {
         for (int row = 0; row < rowSize; row++) {
             for (int col = 0; col < rowSize; col++) {
-                gameBoard[row][col] = new TicTacToeTile(row, col);
+                gameBoard[row][col] = new TicTacToeTile();
             }
         }
     }
